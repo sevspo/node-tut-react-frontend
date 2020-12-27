1f978e39a094904df9c581c43f4dd8cae3e05fcd
Severin Spoerri <severin.spoerri@outlook.com>
2020-12-27 18:14:11 +0100

    user status

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -22,7 +22,9 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch("URL")
+    fetch("http://localhost:8080/auth/status", {
+      headers: { Authorization: "Bearer " + this.props.token },
+    })
       .then((res) => {
         if (res.status !== 200) {
           throw new Error("Failed to fetch user status.");
@@ -73,7 +75,14 @@ class Feed extends Component {
 
   statusUpdateHandler = (event) => {
     event.preventDefault();
-    fetch("URL")
+    fetch("http://localhost:8080/auth/status", {
+      method: "PUT",
+      headers: {
+        Authorization: "Bearer " + this.props.token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ status: this.state.status }),
+    })
       .then((res) => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
